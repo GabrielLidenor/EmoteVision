35c2f5388af0a4fe4ecae934185663d969c9f0d0
Gabriel Lidenor <765963+GabrielLidenor@users.noreply.github.com>
2026-08-04 13:18:38 -0500

    feat: data loader (#10)

---
+++ src/paths.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+
+# data folder
+RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
+
+# hugging face project data
+REPO_ID = "gabriellidenor/facial_emotion_images"
+
