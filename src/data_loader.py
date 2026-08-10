0b20091f695ccf0f5cb6ef05cc0fcb0b4e4d8509
Gabriel Lidenor <765963+GabrielLidenor@users.noreply.github.com>
2026-08-09 19:18:21 -0500

    7 feat implement modular trainer class and unit tests (#13)
    * chore: torch
    
    To use for training data
    
    * feat: train, test data and dataloaders
    
    Methods to load test and train data
    also important to have dataloader methods for train and test data to use
    torch, so we can take advantage of the GPU for training the model.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,16 @@
 from datasets import load_dataset, load_from_disk
 from src.paths import RAW_DATA_DIR, REPO_ID
+import torch
+from torch.utils.data import TensorDataset, DataLoader as TorchDataLoader
 
 class DataLoader:
-    def __init__(self):
+    def __init__(self, test_size = 0.2, batch_size = 32, seed = 42, num_workers = 2):
         self.local_path = RAW_DATA_DIR
         self.repository_id = REPO_ID
+        self.test_size = test_size
+        self.batch_size = batch_size
+        self.seed = seed
+        self.num_workers = num_workers
 
     def download(self):
         dataset = load_dataset(self.repository_id)
@@ -17,3 +23,48 @@ class DataLoader:
 
         return load_from_disk(str(self.local_path))
 
+    def get_train_data(self):
+        train_data = self.load()['train']
+
+        X_train = train_data['image']
+        y_train = train_data['label']
+
+        return X_train, y_train
+
+    def get_train_loader(self):
+        X_train, y_train = self.get_train_data()
+
+        X_tensor = torch.tensor(X_train, dtype = torch.float32)
+        y_tensor = torch.tensor(y_train, dtype = torch.long)
+
+        dataset = TensorDataset(X_tensor, y_tensor)
+
+        return TorchDataLoader(
+                dataset,
+                batch_size = self.batch_size,
+                shuffle = True,
+                num_workers = self.num_workers
+                )
+
+    def get_test_data(self):
+        test_data = self.load()['test']
+
+        X_test = test_data['image']
+        y_test = test_data['label']
+
+        return X_test, y_test
+
+    def get_test_loader(self):
+        X_test, y_test = self.get_test_data()
+
+        X_tensor = torch.tensor(X_test, dtype = torch.float32)
+        y_tensor = torch.tensor(y_test, dtype = torch.long)
+
+        dataset = TensorDataset(X_tensor, y_tensor)
+
+        return TorchDataLoader(
+                dataset,
+                batch_size = self.batch_size,
+                shuffle = False,
+                num_workers = self.num_workers
+                )
