833650a456ed11b71d715105f71479585a67f2a8
Gabriel Lidenor <oxentestudios@gmail.com>
2026-08-18 12:51:29 -0500

    fix: model performance

--- src/models/facial_recognition_model.py
+++ src/models/facial_recognition_model.py
@@ -1,45 +1,38 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models import resnet50, ResNet50_Weights
 
 class FacialRecognitionModel(nn.Module):
-    def __init__(self, num_classes: int = 10):
+    def __init__(self, embedding_size: int = 512, num_classes: int = 7):
         super().__init__()
-
-        self.num_classes = num_classes
 
         # Pretrained ResNet50 backbone
         self.base_model = resnet50(weights = ResNet50_Weights.DEFAULT)
 
         in_features = self.base_model.fc.in_features
         self.base_model.fc = nn.Identity()
-        # ResNet50 is a pretrained model, which reduces the amount
-        # of time we need to train with our data.
-        # we freeze the layers to prevent our data to make our model
-        # to forget what they pre-trained before.
-        # we should only focus on data that the model cannot read with
-        # its pre-trained data
-        for param in self.base_model.parameters():
-            param.requires_grad = False
 
-        self.dense1 = nn.Linear(in_features, 256)
-        self.dense2 = nn.Linear(256, 128)
-        self.classifier = nn.Linear(128, self.num_classes)
+        for name, param in self.base_model.named_parameters():
+            if "layer4" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
 
-        self.relu = nn.ReLU()
-
+        self.embedding_layer = nn.Linear(in_features, embedding_size)
+        self.classifier = nn.Linear(embedding_size, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Defines the execution pipeline for processing data batches.
+        """Defines the execution pipeline for processing data batches.
+        Returns logits suitable for `nn.CrossEntropyLoss`.
         """
         features = self.base_model(x)
         vector = torch.flatten(features, start_dim = 1)
 
-        embedding1 = self.relu(self.dense1(vector))
-        embedding2 = self.relu(self.dense2(embedding1))
+        embeddings = self.embedding_layer(vector)
+        embeddings = F.relu(embeddings)
 
-        predictions = self.classifier(embedding2)
+        logits = self.classifier(embeddings)
 
-        return predictions
+        return logits
 
