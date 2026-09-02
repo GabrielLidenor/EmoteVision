06c50919416a55bc178acda3c54b04cfcd5880dc
Gabriel Lidenor <oxentestudios@gmail.com>
2026-09-02 16:42:44 -0500

    chore: artifacts folder into paths

--- src/paths.py
+++ src/paths.py
@@ -7,6 +7,9 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 # data folder
 RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
 
+# artifacts folder
+ARTIFACTS_DIR = PROJECT_ROOT / "artifacts"
+
 # hugging face project data
 REPO_ID = "gabriellidenor/facial_emotion_images"
 
